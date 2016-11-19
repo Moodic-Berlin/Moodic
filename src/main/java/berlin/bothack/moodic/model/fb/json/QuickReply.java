84fad7a2496a09077ea16cce7e845c03febae822
vgorin <vgrn@i.com.ua>
2016-11-19 18:24:13 +0200

    further extend fb API usage

--- src/main/java/berlin/bothack/moodic/model/fb/json/QuickReply.java
+++ src/main/java/berlin/bothack/moodic/model/fb/json/QuickReply.java
@@ -1,5 +1,7 @@
 package berlin.bothack.moodic.model.fb.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author vgorin
  *         file created on 11/19/16 2:41 PM
@@ -7,5 +9,21 @@ package berlin.bothack.moodic.model.fb.json;
 
 
 public class QuickReply {
+	@JsonProperty("content_type") public String contentType;
+	public String title;
 	public String payload;
+	@JsonProperty("image_url") public String imageUrl;
+
+	public QuickReply() {
+	}
+
+	public QuickReply(String title) {
+		this(title, title);
+	}
+
+	public QuickReply(String title, String payload) {
+		this.contentType = "text";
+		this.title = title;
+		this.payload = payload;
+	}
 }
