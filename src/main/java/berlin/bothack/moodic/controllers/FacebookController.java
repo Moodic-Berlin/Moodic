906ece5d4287016cb0e83bdbe6e924bfb65222f3
justrocknrolla <Weblogic1>
2016-11-19 09:17:56 -0800

    Added Cognitive Services entry point to FB Controller

--- src/main/java/berlin/bothack/moodic/controllers/FacebookController.java
+++ src/main/java/berlin/bothack/moodic/controllers/FacebookController.java
@@ -1,9 +1,13 @@
 package berlin.bothack.moodic.controllers;
 
 import berlin.bothack.moodic.Conf;
+import berlin.bothack.moodic.enums.Emotion;
 import berlin.bothack.moodic.model.fb.MessageSender;
 import berlin.bothack.moodic.model.fb.QuickReplyBuilder;
 import berlin.bothack.moodic.model.fb.json.*;
+import berlin.bothack.moodic.model.microsoft.cognitive.EmotionResponse;
+import berlin.bothack.moodic.services.LogicService;
+import berlin.bothack.moodic.services.MicrosoftCognitiveService;
 import berlin.bothack.moodic.services.SpotifyService;
 import berlin.bothack.moodic.util.Messages;
 import org.slf4j.Logger;
@@ -27,13 +31,17 @@ public class FacebookController {
 	private final Conf conf;
 	private final MessageSender messageSender;
 	private final Messages messages;
+	private final MicrosoftCognitiveService microsoftCognitiveService;
+	private final LogicService logicService;
 
 	@Autowired
-	public FacebookController(Messages messages, SpotifyService spotifyService, Conf conf, MessageSender messageSender) {
+	public FacebookController(Messages messages, SpotifyService spotifyService, Conf conf, MessageSender messageSender, MicrosoftCognitiveService microsoftCognitiveService, LogicService logicService) {
 		this.spotifyService = spotifyService;
 		this.conf = conf;
 		this.messageSender = messageSender;
 		this.messages = messages;
+		this.microsoftCognitiveService = microsoftCognitiveService;
+		this.logicService = logicService;
 	}
 
 	@RequestMapping(
@@ -126,15 +134,18 @@ public class FacebookController {
 	}
 
 	private Response processImage(String senderId, String imageUrl) throws IOException {
-		// TODO: your custom impl goes here
-		Response response = messageSender.send(senderId, imageUrl);
-		if(response != null) {
-			log.info("message {} sent back to {} successfully!", imageUrl, senderId);
+		try {
+			EmotionResponse emotionResponse = microsoftCognitiveService.retrieveEmotion(imageUrl);
+			log.info("Emotion Response is {}", emotionResponse);
+			Emotion emotion = microsoftCognitiveService.getMostLikableEmotion(emotionResponse);
+			String genre = logicService.emotionToGenre(emotion.name());
+			log.info("Received following quick reply action: {}, corresponding genre is: {}", emotion.name(), genre);
+			messageSender.send(senderId, "Your emotion is " + emotion.name());
+			return messageSender.send(senderId, spotifyService.retrieveSpotifyUrl(spotifyService.randomTrackForGenre(genre)));
+		} catch (Exception ex) {
+			log.debug("Something went really wrong, imageUrl is : {}", imageUrl);
+			return sendNoEmotion(senderId);
 		}
-		else {
-			log.warn("error replying back to {}", senderId);
-		}
-		return response;
 	}
 
 	private Response processQuickReply(String senderId, String payload) throws IOException {
@@ -149,16 +160,16 @@ public class FacebookController {
 		return response;
 	}
 
-	private void sendFooterQuickReply(String senderId) throws IOException {
+	private Response sendFooterQuickReply(String senderId) throws IOException {
 		QuickReplyBuilder builder = QuickReplyBuilder.builder();
 		for (String emotion : spotifyService.listEmotions()) {
 			builder.addQuickReply(emotion);
 		}
-		messageSender.send(senderId, messages.get("dabot.howDoYouFeel"), builder.build());
+		return messageSender.send(senderId, messages.get("dabot.howDoYouFeel"), builder.build());
 	}
 
-	private void sendNoEmotion(String senderId) throws IOException {
-		messageSender.send(senderId, "No Emotion detected, please give it another try!");
+	private Response sendNoEmotion(String senderId) throws IOException {
+		return messageSender.send(senderId, "No Emotion detected, please give it another try!");
 	}
 
 }
