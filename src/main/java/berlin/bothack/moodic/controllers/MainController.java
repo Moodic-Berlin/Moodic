d6d13d5cd4b4c0944871bcf2b8710864451be7b1
xonix <xonixx@gmail.com>
2016-11-19 15:30:03 +0100

    fix

--- src/main/java/berlin/bothack/moodic/controllers/MainController.java
+++ src/main/java/berlin/bothack/moodic/controllers/MainController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html")
     public String getHome() {
-        return "Hi, I'm Moodic bot. " +
-                "Find me at <a href=https://fb.me/Moodic/>fb.me/Moodic</a> or <a href=https://m.me/Moodic>m.me/Moodic</a>."; // TODO: fixme if changed
+        return "<h2>Hi, I'm Moodic bot. " +
+                "Find me at <a href=https://fb.com/Moodic/>fb.com/Moodic</a> or <a href=https://m.me/Moodic>m.me/Moodic</a>." +
+                "</h2>"; // TODO: fixme if changed
     }
     @RequestMapping(value = "/status", method = RequestMethod.GET)
     public String getStatus() {
