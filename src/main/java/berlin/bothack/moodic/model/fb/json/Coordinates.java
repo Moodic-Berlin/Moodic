2aa47308ada9ddbd225e671fceb1e141c10fceb9
justrocknrolla <Weblogic1>
2016-11-19 20:14:50 -0800

    Fuck it's 5am, my brain dying/....

--- src/main/java/berlin/bothack/moodic/model/fb/json/Coordinates.java
+++ src/main/java/berlin/bothack/moodic/model/fb/json/Coordinates.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * Created by Oleksandr Shchetynin on 11/19/2016.
  */
-public class Coordinate {
+public class Coordinates {
     public Double lat;
     @JsonProperty("long") public Double lng;
 }
--- src/main/java/berlin/bothack/moodic/model/fb/json/Payload.java
+++ src/main/java/berlin/bothack/moodic/model/fb/json/Payload.java
@@ -15,5 +15,5 @@ public class Payload {
 	@JsonProperty("template_type") public String templateType;
 	public String text;
 	public List<Button> buttons;
-	public List<Coordinate> coordinates;
+	public Coordinates coordinates;
 }
