171f72d3d8ede598882b630be781b5a84d647b8b
xonix <xonixx@gmail.com>
2016-11-27 14:41:59 +0200

    improve deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+USER=apps
+SERV=ec2-54-218-119-31.us-west-2.compute.amazonaws.com
+
 echo
 echo "BUILD..."
 echo
@@ -10,13 +13,17 @@ echo
 echo "DEPLOY..."
 echo
 
-scp moodic.conf target/moodic.jar xonix@ec2-54-218-119-31.us-west-2.compute.amazonaws.com:~/moodic
+scp moodic.conf target/moodic.jar $USER@$SERV:~/
 
 echo
 echo "RESTART..."
 echo
 
-ssh xonix@ec2-54-218-119-31.us-west-2.compute.amazonaws.com "
+ssh $USER@$SERV "
+if [ ! -f /etc/init.d/moodic ]
+then
+    sudo ln -s /home/apps/moodic.jar /etc/init.d/moodic
+fi
 sudo /etc/init.d/moodic restart
 tail -f /var/log/moodic.log
 "
