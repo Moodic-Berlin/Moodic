fdad27438db8f6a039470e74996ff1e710648913
vgubarkov <xonixx@gmail.com>
2018-01-24 20:39:51 +0200

    deps + cfgs

--- src/main/java/berlin/bothack/moodic/MoodicApplication.java
+++ src/main/java/berlin/bothack/moodic/MoodicApplication.java
@@ -5,10 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * @author vgorin
@@ -19,7 +24,7 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @ComponentScan(basePackages = "berlin.bothack.moodic")
 @EnableScheduling
-public class MoodicApplication {
+public class MoodicApplication implements SchedulingConfigurer {
 	private static final Logger log = LoggerFactory.getLogger(MoodicApplication.class);
 
 	@Autowired
@@ -33,5 +38,15 @@ public class MoodicApplication {
 	void postConstruct() {
 		log.info("FB_VERIFY_TOKEN: {}", conf.FB_VERIFY_TOKEN);
 		log.info("FB_PAGE_ACCESS_TOKEN: {}", conf.FB_PAGE_ACCESS_TOKEN);
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		taskRegistrar.setScheduler(taskExecutor());
+	}
+
+	@Bean(destroyMethod="shutdown")
+	public Executor taskExecutor() {
+		return Executors.newScheduledThreadPool(10);
 	}
 }
